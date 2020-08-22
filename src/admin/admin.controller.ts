a05ef19c5ac49b763188ab323508f672dca7f20f
niqitosiq <nikitashtanko@yahoo.com>
2020-08-22 08:23:38 +0400

    Stash

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AdminService } from './admin.service';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+
+  @Post('/xml')
+  @UseInterceptors(FileInterceptor('file'))
+  parseXML(@UploadedFile() file: File): Promise<boolean> {
+    return this.adminService.checkFile(file);
+  }
+}
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  checkFile(file: File): any {
+    console.log(file);
+    return {
+      status: true,
+    };
+  }
+}
