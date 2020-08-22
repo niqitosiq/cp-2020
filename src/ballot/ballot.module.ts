802cf1f38d04a006a113ecdae0ab71b2dc80df0f
DanielTenebris <danieltenebris@gmail.com>
2020-08-22 09:22:13 +0000

    :arrow_up: Import ballotschema to ballot module

--- src/ballot/ballot.module.ts
+++ src/ballot/ballot.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { BallotController } from './ballot.controller';
 import { BallotService } from './ballot.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { BallotSchema } from './schemas/ballot.schema';
+
 @Module({
+  imports: [MongooseModule.forFeature( [ {name: 'Ballot', schema: BallotSchema} ] ) ],
   controllers: [BallotController],
   providers: [BallotService],
 })
